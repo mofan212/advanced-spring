b14705f0ccb74ab78f69602ef440b04c41c025ba
mofan <cy.mofan@foxmail.com>
2022-12-27 22:09:06 +0800

    @Autowired 后置处理器

--- bean/src/main/java/indi/mofan/bean/a04/Bean1.java
+++ bean/src/main/java/indi/mofan/bean/a04/Bean1.java
@@ -24,6 +24,7 @@ public class Bean1 {
         this.bean2 = bean2;
     }
 
+    @Autowired
     private Bean3 bean3;
 
     @Resource
---
+++ bean/src/main/java/indi/mofan/bean/a04/DigInAutowired.java
@@ -0,0 +1,67 @@
+package indi.mofan.bean.a04;
+
+import lombok.SneakyThrows;
+import org.springframework.beans.PropertyValues;
+import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
+import org.springframework.beans.factory.annotation.InjectionMetadata;
+import org.springframework.beans.factory.config.DependencyDescriptor;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.annotation.ContextAnnotationAutowireCandidateResolver;
+import org.springframework.core.MethodParameter;
+import org.springframework.core.env.StandardEnvironment;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * @author mofan
+ * @date 2022/12/27 21:29
+ */
+public class DigInAutowired {
+    @SneakyThrows
+    public static void main(String[] args) {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        beanFactory.registerSingleton("bean2", new Bean2());
+        beanFactory.registerSingleton("bean3", new Bean3());
+        // @Value
+        beanFactory.setAutowireCandidateResolver(new ContextAnnotationAutowireCandidateResolver());
+        // ${} 的解析器
+        beanFactory.addEmbeddedValueResolver(new StandardEnvironment()::resolvePlaceholders);
+
+        // 查看哪些属性、方法加了 @Autowired，这称之为 InjectionMetadata
+        AutowiredAnnotationBeanPostProcessor postProcessor = new AutowiredAnnotationBeanPostProcessor();
+        postProcessor.setBeanFactory(beanFactory);
+
+        Bean1 bean1 = new Bean1();
+//        System.out.println(bean1);
+//        // 执行依赖注入，@Autowired、@Value
+//        postProcessor.postProcessProperties(null, bean1, "bean1");
+//        System.out.println(bean1);
+
+        Method method = AutowiredAnnotationBeanPostProcessor.class.getDeclaredMethod("findAutowiringMetadata", String.class, Class.class, PropertyValues.class);
+        method.setAccessible(true);
+        InjectionMetadata metadata = (InjectionMetadata) method.invoke(postProcessor, "bean1", Bean1.class, null);
+        // 此处断点
+        System.out.println(metadata);
+
+        // 调用 InjectionMetadata 来进行依赖注入，注入时按类型查找值
+        metadata.inject(bean1, "bean1", null);
+        System.out.println(bean1);
+
+        // 如何按类型查找值
+        Field bean3 = Bean1.class.getDeclaredField("bean3");
+        DependencyDescriptor dd1 = new DependencyDescriptor(bean3, false);
+        Object o1 = beanFactory.doResolveDependency(dd1, null, null, null);
+        System.out.println(o1);
+
+        Method setBean2 = Bean1.class.getDeclaredMethod("setBean2", Bean2.class);
+        DependencyDescriptor dd2 = new DependencyDescriptor(new MethodParameter(setBean2, 0), false);
+        Object o2 = beanFactory.doResolveDependency(dd2, null, null, null);
+        System.out.println(o2);
+
+        Method setHome = Bean1.class.getDeclaredMethod("setHome", String.class);
+        DependencyDescriptor dd3 = new DependencyDescriptor(new MethodParameter(setHome, 0), true);
+        Object o3 = beanFactory.doResolveDependency(dd3, null, null, null);
+        System.out.println(o3);
+    }
+}
