d80062ca5a12f526e1ef79840a262388e887eee4
mofan <cy.mofan@foxmail.com>
2023-01-10 21:07:45 +0800

    修改注释

--- aspectj_02/src/main/java/indi/mofan/A11Application.java
+++ aspectj_02/src/main/java/indi/mofan/A11Application.java
@@ -12,7 +12,7 @@ public class A11Application {
 
     /*
     * 启动时需要添加 JVM 参数：-javaagent:D:\environment\Maven\3.6.3-repository\.m2\repository\org\aspectj\aspectjweaver\1.9.7\aspectjweaver-1.9.7.jar
-    * 其中的 D:\environment\Maven\3.6.3-repository\.m2 指 Maven 仓库地址
+    * 其中的 D:\environment\Maven\3.6.3-repository\.m2 指本地 Maven 仓库地址
     * 除此之外，需要确保本地仓库中存在 1.9.7 版本的 aspectjweaver，否则修改至对应版本
     * */
 
