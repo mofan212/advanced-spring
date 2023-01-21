907e6caea13951f7dec12d74715f36f49ae10f1f
mofan <cy.mofan@foxmail.com>
2023-01-21 12:03:18 +0800

    高级切面转低级切面

---
+++ aop/src/main/java/org/springframework/aop/framework/autoproxy/A17_2.java
@@ -0,0 +1,84 @@
+package org.springframework.aop.framework.autoproxy;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectInstanceFactory;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.aspectj.AspectJMethodBeforeAdvice;
+import org.springframework.aop.aspectj.SingletonAspectInstanceFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author mofan
+ * @date 2023/1/21 11:17
+ */
+public class A17_2 {
+
+    static class Aspect {
+        @Before("execution(* foo())")
+        public void before1() {
+            System.out.println("before1");
+        }
+
+        @Before("execution(* foo())")
+        public void before2() {
+            System.out.println("before2");
+        }
+
+        public void after() {
+            System.out.println("after");
+        }
+
+        public void afterReturning() {
+            System.out.println("afterReturning");
+        }
+
+        public void afterThrowing() {
+            System.out.println("afterThrowing");
+        }
+
+        public Object around(ProceedingJoinPoint pjp) throws Throwable {
+            try {
+                System.out.println("around...before");
+                return pjp.proceed();
+            } finally {
+                System.out.println("around...after");
+            }
+        }
+    }
+
+    static class Target {
+        public void foo() {
+            System.out.println("target foo");
+        }
+    }
+
+    public static void main(String[] args) throws Throwable {
+        // 切面对象实例工厂，用于后续反射调用切面中的方法
+        AspectInstanceFactory factory = new SingletonAspectInstanceFactory(new Aspect());
+        // 高级切面转低级切面类
+        List<Advisor> list = new ArrayList<>();
+        for (Method method : Aspect.class.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(Before.class)) {
+                // 解析切点
+                String expression = method.getAnnotation(Before.class).value();
+                AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+                pointcut.setExpression(expression);
+                // 通知类。前置通知对应的通知类是 AspectJMethodBeforeAdvice
+                AspectJMethodBeforeAdvice advice = new AspectJMethodBeforeAdvice(method, pointcut, factory);
+                // 切面
+                Advisor advisor = new DefaultPointcutAdvisor(pointcut, advice);
+                list.add(advisor);
+            }
+        }
+        for (Advisor advisor : list) {
+            System.out.println(advisor);
+            System.out.println();
+        }
+    }
+}
